cc99f1eaf9daec631dbbf6363719583b8aeea03e
Alex Day <alexdaygjj@gmail.com>
2025-01-14 20:04:47 -0700

    Fix mobile video id extraction and add functionality to update yt-dlp without restarting the container

--- utils/function_calling.py
+++ utils/function_calling.py
@@ -13,7 +13,7 @@ def get_transcription(link: str, gpt: OpenAI):
 
     # First we can clean up the youtube link to get the video id
     if 'youtu.be' in link:  # Mobile links
-        video_id = link.split('.be/')[1].split('?')[0]
+        video_id = link.split("youtu.be/")[1].split("?")[0]
     elif 'youtube.com' in link:  # Desktop links
         video_id = link.split("v=")[1].split("&")[0]
     else:
@@ -27,6 +27,8 @@ def get_transcription(link: str, gpt: OpenAI):
         check = download_audio(formatted_link)
         if not check:
             log.error(f"Failed to download audio from {formatted_link} after {num_tries} tries")
+            if num_tries == 1:
+                _ = update_yt_dlp()
             num_tries += 1
         else:
             break
@@ -48,6 +50,13 @@ def get_transcription(link: str, gpt: OpenAI):
     os.remove("downloaded_audio.m4a")
 
     return transcription
+
+
+def update_yt_dlp():
+    """Update the yt-dlp package and return whether the return code is == 0"""
+    log.info("Attempting to update yt-dlp")
+    output = subprocess.run(["pip", "install", "-U", "yt-dlp"])
+    return output.returncode == 0
 
 
 def download_audio(link: str) -> bool:
@@ -75,4 +84,5 @@ if __name__ == "__main__":
         config = load(yml, Loader=Loader)
     gpt: OpenAI = OpenAI(api_key=config["openai_api_key"])
 
-    log.info(get_transcription("https://www.youtube.com/watch?v=dQw4w9WgXcQ", gpt))
+    log.info(get_transcription("https://www.youtube.com/watch?v=FHt6glkEM9A&ab_channel=ForbesBreakingNews", gpt))
+    log.info(get_transcription("https://youtu.be/FHt6glkEM9A?si=r1-07kwNLUJdlrYB", gpt))
